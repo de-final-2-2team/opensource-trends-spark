5f14fca4f8996e4b9bceaccc36479f6b042e558c
ksh1357 <sanghee1357@naver.com>
2023-08-16 22:47:33 +0900

    [feat][#3] meta 데이터 저장 - extract, load 함수 구현 - exception_handler 추가

--- dags/github_api.py
+++ dags/github_api.py
@@ -1,15 +1,48 @@
-from sre_parse import State
-from requests.exceptions import HTTPError
 from airflow import DAG
 from airflow.models import Variable
-from airflow.decorators import task, task_group
-
+from airflow.decorators import task
+from airflow.exceptions import (
+    AirflowException,
+    AirflowBadRequest,
+    AirflowNotFoundException,
+    AirflowFailException
+)
+from airflow.utils.trigger_rule import TriggerRule
 from datetime import datetime
 from datetime import timedelta
 
 import requests
 import logging
 import json
+
+
+request_list = {
+    "lincense": {
+        "url": "https://api.github.com/licenses",
+        "params": {"per_page": 30, "page": 1}
+    },
+    "commit_per_hour": {
+        "name": "시간 당 commit 수",
+        "url": "https://api.github.com/repos/{OWNER}/{REPO}/stats/punch_card"
+    },
+    "release": {
+        "url": "https://api.github.com/repos/{OWNER}/{REPO}/tags",
+        "params": {"per_page": 30, "page": 1}
+    },
+    "project": {
+        "url": "  https://api.github.com/repos/{OWNER}/{REPO}/projects",
+        "params": {"state": "all", "per_page": 30, "page": 1}
+    },
+    "contributor_activity": {
+        "url": "https://api.github.com/repos/{OWNER}/{REPO}/stats/code_frequency"
+    }, 
+    "community_profile": {
+        "url": "https://api.github.com/repos/geekan/{OWNER}/{REPO}/profile"
+    },
+    "language": {
+        "url": "https://api.github.com/repos/{OWNER}/{REPO}/languages"
+    }
+}
 
 
 def get_header():
@@ -22,51 +55,33 @@ def get_header():
     }
 
 
-def exception_parser(context):
-    """
-    A function that checks the class name of the Exception thrown.
-    
-    Different exceptions trigger behaviour of allowing the task to fail, retry or succeed
-    """
-    ti = context["task_instance"]
-    exc = context.get('exception')
-    if isinstance(exc, HTTPError):
-        logging.error(repr(exc))
-        if exc.response.status_code == 401:
-            logging.warning("Retry Task")
-        elif exc.response.status_code == 404:
-            ti.set_state(State.SUCCESS)
-        else:
-            ti.set_state(State.FAILED)
-    else:
-        logging.error("Unknown Error. " + repr(exc))
-        ti.set_state(State.FAILED)
-
-
-@task(
-    description="api 요청 결과를 반환",
-    retries=1,
-    retry_delay=timedelta(seconds=3),
-    on_retry_callback=exception_parser,
-)
-def extract(kind, url, params=None):
+@task(trigger_rule=TriggerRule.ONE_FAILED, retries=1)
+def extract(kind):
     try:
         logging.info(f"[{kind}] 데이터 수집 시작")
         headers = get_header()
+        url = request_list.get(kind, {}).get('url')
+        params= request_list.get(kind, {}).get('params')
         response = requests.get(url=url, 
                                 headers=headers,
                                 params=params)
         # 200이 아닌 경우 에러 발생
-        response.raise_for_status()
-        return response.json()
+        if response.status_code == 200:
+            return response.json()
+        elif response.status_code == 400:
+            raise AirflowBadRequest(f"[{kind}] 데이터 수집 실패\\n" + response.content)
+        elif response.status_code == 401:
+            raise AirflowException(f"[{kind}] 데이터 수집 실패\\n" + response.content)
+        elif response.status_code == 404:
+            return AirflowNotFoundException(f"[{kind}] 데이터 수집 실패\\n" + response.content)
+        else:
+            raise AirflowFailException(f"[{kind}] 데이터 수집 실패\\n" + response.content)
     except Exception as e:
         logging.error(f"[{kind}] 데이터 수집 실패\\n" + repr(e))
         raise e
 
 
-@task(
-    description="json 파일 형태로 저장",
-)
+@task()
 def load_as_json(kind, content):
     with open(f"./{kind}.json", 'w') as f:
         json.dump(content, f)
@@ -92,14 +107,11 @@ DAG 설정
     - False: 이전 DAG가 실행 중이더라도 예정대로 다음 DAG를 실
 """
 with DAG(
-    dag_id='github_meta',
-    description='github API meta 데이터 저장',
+    dag_id='meta',
     start_date=datetime(2023, 8, 15, 21),  # 날짜가 미래인 경우 실행이 안됨
-    schedule='10 * * * *',
+    schedule='*/10 * * * *',
     max_active_runs=1,
     catchup=False,
 ) as dag:
-    url = Variable.get("meta_url")
-    params = Variable.get("meta_params")
-
-    load_as_json(dag.dag_id, extract(dag.dag_id, url, params))
+    response = extract('lincense')
+    load_as_json('lincense', response)
